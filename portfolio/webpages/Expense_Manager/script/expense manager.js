db2db63c1217633b73d62b80a1f265fdcdf16af1
Sanjay Majhi <47259000+sanjaymajhi@users.noreply.github.com>
2019-07-17 19:35:42 +0530

    Update expense manager.js

--- portfolio/webpages/Expense_Manager/script/expense manager.js
+++ portfolio/webpages/Expense_Manager/script/expense manager.js
@@ -1,31 +1,70 @@
-let submit=document.querySelector("#submit");
+
 let obj={};
+let mem={};
+
+let mem_name=document.querySelector(".mem_name");
+
+let app=document.querySelector(".app");
+
+let mem_name_input=document.querySelector("#mem_name_input");
+let mem_name_input_add=document.querySelector("#mem_name_input_add");
+let mem_name_input_submit=document.querySelector("#mem_name_input_submit");
+mem_name_input_add.onclick=function(){
+    mem[mem_name_input.value]=0;
+
+    let option=document.createElement("option");
+    let paidby=document.querySelector("#paidby");
+    option.value=mem_name_input.value;
+    option.textContent=mem_name_input.value;
+    paidby.appendChild(option);
+
+    let para=document.createElement("p");
+    let cashout=document.querySelector(".cashout");
+    para.setAttribute('id','c_'+mem_name_input.value);
+    cashout.appendChild(para);
+
+    let para_equate=document.createElement("p");
+    let equate=document.querySelector(".equate");
+    para_equate.setAttribute('id','e_'+mem_name_input.value);
+    equate.appendChild(para_equate);
+
+    mem_name_input.value='';
+}
+
+mem_name_input_submit.onclick=function(){
+  mem_name.innerHTML='';
+  app.style.visibility='visible';
+}
+
+
+
+let submit=document.querySelector("#submit");
 let total=0;
-let total_sanjay=0;
-let total_ashish=0;
-let total_ayan=0;
-let total_sanju=0;
-let total_aakash=0;
 submit.onclick=function(){
+  let len=0;
+  for(i in mem){
+    if(mem.hasOwnProperty(i)){
+      len++;
+    }
+  }
   let item=document.querySelector("#item");
   let price=document.querySelector("#price");
   let paidby=document.querySelector("#paidby");
   obj[item.value]={'price':price.value,'paid_by':paidby.value};
   total+=parseInt(price.value);
-  eval("total_"+paidby.value+"+="+price.value);
+  mem[paidby.value]+=parseInt(price.value);
   document.querySelector("#total").textContent='Total Expense : '+total;
   let list=document.querySelector("#items");
   let li=document.createElement("li");
   li.textContent=item.value+" bought by "+paidby.value+" of price : "+price.value;
   list.appendChild(li);
   document.querySelector("#c_"+paidby.value).textContent="Cash given by "+paidby
-  .value+" = "+eval("total_"+paidby.value);
-  document.querySelector("#avgex").textContent="Expense per Head = "+(total/5);
-  document.querySelector("#e_sanjay").textContent="Cash needed to pay by sanjay"+" = "+(total/5-total_sanjay);
-  document.querySelector("#e_ashish").textContent="Cash needed to pay by ashish"+" = "+(total/5-total_ashish);
-  document.querySelector("#e_ayan").textContent="Cash needed to pay by ayan"+" = "+(total/5-total_ayan);
-  document.querySelector("#e_sanju").textContent="Cash needed to pay by sanju"+" = "+(total/5-total_sanju);
-  document.querySelector("#e_aakash").textContent="Cash needed to pay by aakash"+" = "+(total/5-total_aakash);
+  .value+" = "+mem[paidby.value];
+  document.querySelector("#avgex").textContent="Expense per Head = "+(total/len);
+  for(let i in mem){
+    if(mem.hasOwnProperty(i)){
+    document.querySelector("#e_"+i).textContent="Cash needed to pay by "+i+" = "+(total/len-mem[i]);
+  }}
   item.value='';
   price.value='';
   item.focus();
